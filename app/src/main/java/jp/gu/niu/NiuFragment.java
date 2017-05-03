0e957340e183e248208902eb6346176d07535b56
keiichi <xkei4dev@gmail.com>
2017-05-04 08:36:44 +0900

    init niu

---
+++ app/src/main/java/jp/gu/niu/NiuFragment.java
@@ -0,0 +1,46 @@
+package jp.gu.niu;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by xkei4 on 2017/05/02.
+ */
+
+public class NiuFragment extends Fragment {
+
+    Thinker thinker = new Thinker();
+
+    // Viewが生成し終わった時に呼ばれるメソッド
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // TextViewをひも付けます
+
+        Toast.makeText(getActivity(), "にう！", Toast.LENGTH_SHORT).show();
+    }
+
+//    public String getReply(String userWord) {
+//        return thinker.getReply(userWord);
+//    }
+
+    public void say() {
+        Toast.makeText(getActivity(), "にう！", Toast.LENGTH_SHORT).show();
+    }
+
+    public String getWill(String word) {
+        if(!word.equals("") && word != null) {
+            return thinker.will(word);
+        } else
+            return "";
+    }
+
+}
